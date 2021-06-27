a0ea016719e7bd2897ce172186608c8847aa58d7
culi <culitif@tuta.io>
2021-06-26 19:39:00 -0700

    create rest of tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,6 +14,11 @@ describe('friends routes', () => {
     birthday: '2020-05-13'
   };
 
+  const noodle = {
+    name: 'Noodle',
+    birthday: '2019-11-03'
+  };
+
   test('add a friend via POST', async () => {
     const res = await request(app)
       .post('/api/v1/friends')
@@ -22,23 +27,59 @@ describe('friends routes', () => {
 
     expect(res.body).toEqual({
       ...cami,
-      id: '1'
+      id: expect.any(Number)
     });
   });
 
   test('get a friend by id via GET', async () => {
+    await Friend.insert(cami);
 
+    const res = await request(app).get('/api/v1/friends/1');
+
+    expect(res.body).toEqual({
+      ...cami,
+      id: expect.any(Number)
+    });
   });
 
   test('get all friends via GET', async () => {
+    await Friend.insert(cami);
+    await Friend.insert(noodle);
 
+    const res = await request(app).get('/api/v1/friends/');
+
+    expect(res.body).toEqual([
+      {
+        ...cami,
+        id: expect.any(Number)
+      },
+      {
+        ...noodle,
+        id: expect.any(Number)
+      }
+    ]);
   });
 
   test('modify a friend via PUT', async () => {
+    await Friend.insert(cami);
 
+    const res = await request(app)
+      .put('/api/v1/friends/1')
+      .send({ ...cami, id: 1, name: 'Cami' })
+    ;
+
+    expect(res.body.name).toEqual('Cami');
   });
 
   test('remove a friend via DELETE', async () => {
+    await Friend.insert(cami);
 
+    const res = await request(app).delete('/api/v1/friends/1');
+
+    expect(res.body).toEqual({ ...cami, id: 1 });
+    
+    const res2 = await request(app).get('/api/v1/friends/');
+
+    expect(res2.body).not.toEqual(expect.arrayContaining([res]));
   });
 });
