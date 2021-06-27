01942c881ea91e9e92c02cef09740aae59355c72
culi <culitif@tuta.io>
2021-06-26 19:21:44 -0700

    red test for POST friend

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,43 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Friend from '../lib/models/Friend.js';
 
-describe('demo routes', () => {
+describe('friends routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  const cami = {
+    name: 'Chamomile',
+    birthday: '2020-05-13'
+  };
+
+  test('add a friend via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/friends')
+      .send(cami)
+    ;
+
+    expect(res.body).toEqual({
+      ...cami,
+      id: '1'
+    });
+  });
+
+  test('get a friend by id via GET', async () => {
+
+  });
+
+  test('get all friends via GET', async () => {
+
+  });
+
+  test('modify a friend via PUT', async () => {
+
+  });
+
+  test('remove a friend via DELETE', async () => {
+
   });
 });
